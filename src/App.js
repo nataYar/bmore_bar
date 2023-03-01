6c94cd910314cd285c54db75e0e1819e47051a9e
nataliaYarr <n.yarysheva@gmail.com>
2023-02-28 19:50:01 -0800

    nav bar, slide-1-2

--- src/App.js
+++ src/App.js
@@ -1,73 +1,128 @@
 import React from "react";
-import { useLayoutEffect, useRef } from "react";
-import './App.css';
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-gsap.registerPlugin(ScrollTrigger);
+import { useState, useRef, useEffect } from "react";
+import './Styles/App.css';
+import './Styles/styles.css';
+import './Styles/nav.css';
+import Animations from "./animations.js";
+import { default as logo } from './images/logo_white.png';
 
 function App() {
+  const [navClicked, setNavclicked] = useState(false)
   const slide1Ref = useRef();
-  const tlSlide1 = useRef();
+  const slide2Ref = useRef();
+  const textSlide1Ref = useRef();
+  
+  const navRef = useRef(); 
+  const addressContainerRef = useRef();
 
-  useLayoutEffect(() => {
-    const ctx = gsap.context((self) => {
-      const lines = self.selector(".popup-text");
-      tlSlide1.current = gsap
-        .timeline()
-        .fromTo('.logo', { y: '20px', opacity: 0, duration: 1, }, { y: '0', opacity: 1, duration: 1.8 })
-        .fromTo(lines, { y: '20px', opacity: 0 }, { y: '0', opacity: 1, duration: 1, stagger: .5 } , .2)
-    }, slide1Ref); // <- Scope!
-    return () => ctx.revert(); // <- Cleanup!
-  }, []);
-
-
-  // useLayoutEffect(() => {
-  //   const ctx = gsap.context((self) => {
-  //     const lines = self.selector(".popup-text");
-  //     tlSlide1.current = gsap
-  //       .timeline()
-  //       .fromTo(lines[0], { x: '-20px', opacity: 1 }, { x: '0', opacity: 1, duration: 1, stagger: .5 } )
-  //       // .to(lines[1], { x: -120, rotation: -360 }, "<")
-  //       // .to(lines[2], { y: -166 })
-  //       .reverse();
-  //   }, slide1Ref); // <- Scope!
-  //   return () => ctx.revert(); // <- Cleanup!
-  // }, []);
-
+  function openNav () {
+    setNavclicked(!navClicked)
+    console.log('You clicked nav.');
+  }
 
   return (
-    <div className="app">
+    <main className="app">
+      <Animations slide1Ref={slide1Ref}  slide2Ref={slide2Ref} addressContainerRef={addressContainerRef} textSlide1Ref={textSlide1Ref} />
       <header>
-        {/* logo, navigation  */}
-      </header>
-      <main className='app_background'>
-        <article className='slide1-container slide' ref={slide1Ref}>
-          {/* <h1>JavaScript Basics</h1> */}
-          <div className='slide1_logo_container'>
-           <div className="logo"></div>
+        <div className={ navClicked ? 'icon nav-icon open' : 'icon nav-icon'}
+         ref={navRef} onClick={() => openNav()}>
+         <span ></span>
+          <span ></span>
+          <span ></span>
+          
+          <span ></span>
+          <span ></span>
+          <span ></span>
+          
+          <span ></span>
+          <span ></span>
+          <span ></span>
           </div>
-          <p className="popup-text">BE KIND</p>
-          <p className="popup-text">BE STRONG</p>
-          <p className="popup-text">BE MORE</p>
-          {/* <section>
-              <h2>Syntax Basics</h2>
-              <p>Understanding statements, variable naming, whitespace...</p>
-          </section> */}
-          <section>
-              <h2>Operators</h2>
-              <p>Operators allow you to manipulate values...</p>
-          </section>
-          <section>
-              <h2>Conditional Code</h2>
-              <p>Sometimes you only want to run a block of code under certain conditions...</p>
-          </section>
-        </article>
-      </main>
-      
-      <footer>
+      </header>
+      {/* <div className="black_background"/> */}
+      <div className="app_background"></div>
+      <article className='app_content'>
+
+        <section className="slide-100-100 slide1 flex-col-center" ref={slide1Ref} >
+          {/* <h1>JavaScript Basics</h1> */}
+          
+          <h1 className='slide1_logo_container'>
+            <img className='logo' src={logo} alt="bmore protein bar" />
+          </h1>
+
+          <div className="text-block flex-col-center" ref={textSlide1Ref}>
+            <p className="popup-text font-reg" >BE KIND</p>
+            <p className="popup-text font-reg" >BE STRONG</p>
+            <p className="popup-text font-reg" >BE MORE</p>
+          </div>
+          
+          <div className="slide1-bottom-container flex-col-center">
+            <div className="horizontal-line"></div>
+            <div className="btn-container">
+              <div className="btn btn-menu">
+                <span>MENU</span>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className='slide-100-100 slide2 flex-col-left padding-5' id='slide2' ref={slide2Ref}>
+          <div className=""  ref={addressContainerRef}>
+            <p className="popup-text font-regular ">
+                <span>Phone:</span>
+                <br/>
+                (213) 306-0257
+              </p>
+            <p className="popup-text font-regular ">
+              <span>Address:</span>
+              <br/> 
+              707 E Ocean Blvd D Long Beach, CA 90802
+              </p>
+            </div>
+
+          <div className="google-maps">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3316.7700278037464!2d-118.18831427199875!3d33.76660811890422!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dd310b3a6bbf29%3A0xcb4689f44a64d336!2sBmore%20Protein%20Pub!5e0!3m2!1sen!2sus!4v1677538487272!5m2!1sen!2sus" 
+            allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+          </div>
+         
+          <div className="schedule width-100">
+            <div className="flex-row  workday">
+              <div>Mon-Tue</div> 
+              <div className="time">Closed</div>
+              </div>
+            <div className="flex-row workday">
+              <div>Wed</div>
+              <div className="time">11:00 AM - 5:00 PM</div>
+              </div>
+            <div className="flex-row workday">
+              <div>Thu</div>
+              <div>11:00 AM - 5:00 PM</div>
+              </div>
+            <div className="flex-row workday">
+              <div>Fri</div>
+              <div className="time">
+                <div>11:00 AM - 3:00 PM</div>
+                <div>5:00 PM - 8:30 PM</div>
+              </div>
+              </div>
+            <div className="flex-row workday">
+              <div>Sat</div>
+              <div className="time">
+                <div>10:00 AM - 2:00 PM</div>
+                <div>4:30 PM - 8:30 PM</div>
+              </div>
+              </div>
+            <div className="flex-row workday">
+              <div>Sun</div>
+              <div className="time">10:00 AM - 6:00 PM</div>
+            </div>
+          </div>
+        </section>
+      </article>
+      <footer className="padding-5">
           Footer information, links, etc.
       </footer>
-    </div>
+    </main>
   );
 }
 
---
+++ src/animations.js
@@ -0,0 +1,69 @@
+import { useLayoutEffect } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
+const Animations = ({slide1Ref, textSlide1Ref, slide2Ref, addressContainerRef }) => {
+ 
+  useLayoutEffect(() => {
+    let slide = gsap.utils.selector(slide1Ref);
+    gsap.timeline({ 
+        defaults: {
+        duration: 1.2,
+        ease: "power4.inout"
+        } 
+    })
+    .fromTo('.app_background', { scale: 1.5,  opacity: 0 }, {scale: 1,  opacity: 1 })
+    .fromTo('.logo', { y: '60px', scale: .5, opacity: 0, }, { scale: 1, y: '0', opacity: 1}, "<")
+    .fromTo(slide(".popup-text"), { y: '20px', opacity: 0 }, { y: '0', opacity: 1, stagger: .3 }, "<");
+    
+    // let ctx = gsap.context(() => {
+    //   gsap.timeline({ 
+    //     defaults: {
+    //     duration: 1.2,
+    //     ease: "power4.inout"
+    //     }
+    //   })
+    //   .fromTo(".popup-text", { y: '20px', opacity: 0 }, { y: '0', opacity: 1, stagger: .3 }, "+=.5");
+    // }, textSlide1Ref); // <- IMPORTANT! Scopes selector text
+    // return () => ctx.revert(); // cleanup
+  }, []);
+
+
+  useLayoutEffect(() => {
+    gsap.timeline({ 
+        defaults: {
+        duration: 1,
+        ease: "power1.inout"
+        },
+        scrollTrigger: {
+            trigger: document.getElementsByClassName('.slide2'),
+            start: '30% 50%',
+            scrub: true,
+        }
+    })
+    .fromTo('.app_background', {
+        // backgroundPosition: '100% 100%', 
+        // scale: 1
+    },
+    { 
+    //   backgroundPosition: '70% 100%', 
+    //   scale: 1.2, 
+    })
+    const ctx2 = gsap.context(() => {
+    gsap.timeline({ 
+        defaults: {
+        duration: 1,
+        ease: "power2.in"
+        },
+        scrollTrigger: {
+            trigger: addressContainerRef.current,
+            start: '0% 100%',
+        }
+    })
+    .fromTo(".popup-text", { y: '20px', opacity: 0 }, { y: '0', opacity: 1, stagger: .2 })
+    }, addressContainerRef);
+    return () => ctx2.revert(); 
+  }, []);
+}
+export default Animations;
